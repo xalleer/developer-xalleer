66e1e6a8cff64997b833ef5b24216ce1361b7ff4
Sviatoslav Yaroshenko <quex.modest@gmail.com>
2023-02-23 12:41:02 +0200

    My site for portfolio

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const telegram = document.querySelector('.icon-telegram');
+const gitHub = document.querySelector('.icon-github-squared');
+const linkedin = document.querySelector('.icon-linkedin');
+const instagram = document.querySelector('.icon-instagram');
+
+
+telegram.onclick = function(){
+    window.location.href = 'https://t.me/xalleer';
+}
+
+gitHub.onclick = function(){
+    window.location.href = 'https://github.com/xalleer';
+}
+
+linkedin.onclick = function(){
+    window.location.href = 'https://www.linkedin.com/in/sviatoslav-yaroshenko-7a96421b7/';
+}
+
+instagram.onclick = function(){
+    window.location.href = 'https://www.instagram.com/xalleer';
+}
